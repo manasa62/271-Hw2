2fde75f4b6214d3effe6dc4bfaf65222b21c35d6
Manasa <manasa@MANASA-PC.(none)>
2011-02-10 11:27:12 -0800

    last one

--- src/hw2/GFSConstants.java
+++ src/hw2/GFSConstants.java
@@ -7,7 +7,7 @@ public class GFSConstants {
 	public static final int FileServerPort = 4321;
 	public static final int RouterListenPort = 1234;
 	public static final int RouterSendPort = 7895;
-	public static final String routerName = "brain.cs.ucsb.edu";
+	public static final String routerName = "dizzy.cs.ucsb.edu";
 	public static final String fileServerName = "snoopy.cs.ucsb.edu";
 	public static final int ClientPort = 6565;
 	public static  int pid = 0;
